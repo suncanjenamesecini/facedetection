e4a032da14297346fc2383be8c432c24e70ab33a
Milica Babic <72348168+suncanjenamesecini@users.noreply.github.com>
2020-10-15 22:38:09 +0200

    Updates

---
+++ precache-manifest.6a7610a5b92f46d8f348597c1f8b744e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c93590e8f1bcbde1211c125eb5e2e08",
+    "url": "/facedetectionnew/index.html"
+  },
+  {
+    "revision": "5efe06212d769ae9bc6e",
+    "url": "/facedetectionnew/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4523ca9ae8ae822257d2",
+    "url": "/facedetectionnew/static/css/main.29ea7792.chunk.css"
+  },
+  {
+    "revision": "5efe06212d769ae9bc6e",
+    "url": "/facedetectionnew/static/js/2.9d340d11.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/facedetectionnew/static/js/2.9d340d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4523ca9ae8ae822257d2",
+    "url": "/facedetectionnew/static/js/main.19024fcf.chunk.js"
+  },
+  {
+    "revision": "71c6b733d153357a5b9a",
+    "url": "/facedetectionnew/static/js/runtime-main.a926346d.js"
+  }
+]);
